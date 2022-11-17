cfa43f568dbafcdad44f9c0333a05da78d1acf30
Paul <fpaul847@gmail.com>
2022-11-17 08:55:48 +0100

    additional function dividNumber

--- src/config.js
+++ src/config.js
@@ -6,3 +6,13 @@ function addNumber (num1,num2) {
 let numbers = addNumber(3,7);
 
 console.log(numbers);
+
+
+function divideNumber (number1, number2) {
+
+    return number1/number2;
+}
+
+let myNumber = divideNumber(50,2);
+
+console.log(myNumber);
