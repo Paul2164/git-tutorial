11871146dc157f1a11cae4f9a89c7913aff7a22c
Paul <fpaul847@gmail.com>
2022-11-17 09:28:18 +0100

    Version 1

--- src/config.js
+++ src/config.js
@@ -8,11 +8,11 @@ let numbers = addNumber(3,7);
 console.log(numbers);
 
 
-function divideNumber (number1, number2) {
+function powerNumber (number1, number2) {
 
-    return number1/number2;
+    return number1**number2;
 }
 
-let myNumber = divideNumber(50,2);
+let myNumber = powerNumber(50,2);
 
 console.log(myNumber);
